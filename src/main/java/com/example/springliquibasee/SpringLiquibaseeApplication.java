9b2c86b0892d8c43006a29d6faafe715fc4af0c7
Gias Uddin <mdgiashu069@gmail.com>
2023-06-16 18:27:37 +0600

    Set up liquibase

---
+++ src/main/java/com/example/springliquibasee/SpringLiquibaseeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springliquibasee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquibaseeApplication.class, args);
+    }
+
+}
