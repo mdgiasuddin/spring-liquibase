f169deceae4eefa7a0c01f0a80b0ef2742aea88f
Giash Uddin <mdgiashu069@gmail.com>
2023-09-24 19:29:29 +0600

    Added redis cache

--- src/main/resources/db/changelog/changes/202307091-create-person-table.sql
+++ src/main/resources/db/changelog/changes/202307091-create-person-table.sql
@@ -7,3 +7,8 @@ create table if not exists person (
     nationality varchar(255) not null,
     constraint pk_person primary key(id)
 );
+
+comment on column person.id is 'Auto incremented id';
+comment on column person.name is 'Person name will be stored';
+comment on column person.nationality is 'Person country of birth';
+comment on table person is 'Will be used to store details of a person';
